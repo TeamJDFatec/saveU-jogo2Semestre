68a0f1e3583c250725147cc9a41169746ebd6f9b
Victor Silva <matheusvictor5019@gmail.com>
2022-11-29 21:58:59 -0300

    Iniciado menu. Já está trocando de tela e saindo

--- Assets/Scripts/Bacteria.cs
+++ Assets/Scripts/Bacteria.cs
@@ -14,7 +14,7 @@ public class Bacteria : MonoBehaviour
     // Atribuindo pontos por matar os inimigos
     public int pontos;
     public GameObject cp;
-
+    
     // Start is called before the first frame update
     void Start()
     {
